1d78fe29ae67c5750dbc53b2f0c7e893ebdc4b26
Arthur Henningfield <144608814+kylatae@users.noreply.github.com>
2023-12-05 08:13:33 -0600

    Initial setup and rotating cube test

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './Cube.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <div class="cube"> 
+          <div class="box box1">
+            <a target="_blank" href="https://github.com/kylatae/employee_tracker"> 
+              <img src="./img/employeeTracker.png" alt="employee tracker page" /> 
+            </a>
+          </div> 
+
+          <div class="box box2">
+            <a target="_blank" href="https://franklinbrad.github.io/robot-murder-mystery/"> 
+              <img src="./img/roboMurderMystery.png" alt="robo murder mystery web game" />
+            </a> 
+          </div> 
+
+          <div class="box box3"> 
+            <a target="_blank" href="https://saleblazers.fandom.com/wiki/Saleblazers_Wiki">
+              <img src="./img/saleBlazers.png" alt="saleblazers wikipedia at fandom" /> 
+            </a>
+          </div> 
+
+          <div class="box box4"> 
+            <a target="_blank" href="https://kylatae-save-a-book-a485871b6375.herokuapp.com/">
+              <img src="./img/saveABook.png" alt="save a book page" /> 
+            </a>
+          </div> 
+
+          <div class="box box5"> 
+            <a target="_blank" href="https://github.com/kylatae/social-network/tree/main/assets">
+              <img src="./img/socialNetwork.png" alt="social network framework" /> 
+            </a>
+          </div> 
+
+          <div class="box box6"> 
+            <a target="_blank" href="/">
+              <img src="./img/devCard.png" alt="arthur henningfield quick info card" /> 
+            </a>
+          </div> 
+        </div> 
+      </div>
+
+    </>
+  )
+}
+
+export default App
